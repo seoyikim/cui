89fc6c2d6eab2d335300070207896551efb6588e
kaylee105 <syungkim1005@gmail.com>
2025-07-22 17:23:24 +0900

    v1.0.1

--- packages/ui-docs/next.config.js
+++ packages/ui-docs/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['ui-core'],
+    // Netlify 배포를 위한 설정
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true
+    }
 };
 
 module.exports = nextConfig;
