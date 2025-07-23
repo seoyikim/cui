fe7644e06aab6012dda0bee4c4d2382335dd22ca
kaylee105 <syungkim1005@gmail.com>
2025-07-23 12:35:41 +0900

    v1.0.2

--- packages/ui-docs/pages/components/checkbox.jsx
+++ packages/ui-docs/pages/components/checkbox.jsx
@@ -1,7 +1,8 @@
 export default function CheckboxDemoPage() {
   return (
     <div>
-      <h1>Checkbox 컴포넌트</h1>
+      <h2>Checkbox 컴포넌트</h2>
+      <br />
       <p>Checkbox 컴포넌트 데모 페이지입니다.</p>
       <p>추후 Checkbox 컴포넌트가 개발되면 여기에 데모가 표시됩니다.</p>
     </div>
--- packages/ui-docs/pages/components/form.jsx
+++ packages/ui-docs/pages/components/form.jsx
@@ -1,7 +1,8 @@
 export default function FormDemoPage() {
   return (
     <div>
-      <h1>Form 컴포넌트</h1>
+      <h2>Form 컴포넌트</h2>
+      <br />
       <p>Form 컴포넌트 데모 페이지입니다.</p>
       <p>추후 Form 컴포넌트가 개발되면 여기에 데모가 표시됩니다.</p>
     </div>
